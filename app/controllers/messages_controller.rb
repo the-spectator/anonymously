f96344d95ccd6eb132e6e9ed4eeee352e19c3e30
TanyaSaroha <tanyasaroha@gmail.com>
2018-04-25 15:47:51 +0530

    [Tanya] Like and unlock features added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,6 +19,22 @@ class MessagesController < ApplicationController
     end
   end
 
+  def unlock
+    id = params[:id].to_i
+    current_message = current_user.received_messages.find(id)
+    current_message[:is_unlocked] = 1
+    current_message.save!
+    redirect_to dashboard_index_path
+  end
+
+  def like
+    message_id = params[:id].to_i
+    current_message = current_user.received_messages.find(message_id)
+    current_message.is_liked = 1 
+    current_message.save
+    redirect_to dashboard_index_path
+  end
+
   private
 
   def message_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 	resources :messages
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :messages do
+      member do
+        get :unlock
+      end
+    end
+
+    resources :messages do
+      member do
+        get :like
+      end
+    end
+
 end
