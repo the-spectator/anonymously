a25fb84a988665e2180fb4ab9a7db324d3c3d36a
dev <mailakshayb1@gmail.com>
2018-05-02 18:38:50 +0530

    [akshay]rspec controller bug solved

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,7 +1,5 @@
-require_relative 'controller_helpers' 
-	RSpec.configure do |config|
-	  config.include Devise::Test::ControllerHelpers, type: :controller
-	  config.include Devise::Test::ControllerHelpers, type: :view
-	  config.include ControllerHelpers, :type => :controller
-	end
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include ControllerHelpers, :type => :controller
 end
