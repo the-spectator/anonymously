682f3164ea9ef23cf02f88a43c3045f5d5641faa
pegasusflyhigh <bbhuvnaprabhu@gmail.com>
2018-05-02 17:56:36 +0530

    [bhuvna] removed error in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
   root "home#index"
-  devise_for :users
   resources :messages
   resources :messages do
     member do
       get :unlock
     end
   end
-  
-  get 'users/index' 
 
-  root "home#index" 
+  get 'users/index' 
   devise_for :users, path: "sessions"
   resources :users
   
